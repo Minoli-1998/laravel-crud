f74f78a4d51bcac426a866b24bc0f7ee579ee3bf
minoli-1998 <mbhagyagunathilake@gmail.com>
2024-06-04 17:39:00 +0530

    created the basic layout

---
+++ resources/views/products/layout.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel 8 CRUD Application</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <h1>Laravel 8 CRUD Application</h1>
+    <div class="container">
+        <br>
+        @yield('content')
+    </div>
+</body>
+</html>
