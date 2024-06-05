2cedf177fa08f7e953a7ce9807e17c2d35700b7b
minoli-1998 <mbhagyagunathilake@gmail.com>
2024-06-05 14:53:03 +0530

    showing specific product feature added

--- resources/views/products/layout.blade.php
+++ resources/views/products/layout.blade.php
@@ -5,7 +5,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <h1>Laravel 8 CRUD Application</h1>
+    {{-- <h1>Laravel 8 CRUD Application</h1> --}}
     <div class="container">
         <br>
         @yield('content')
